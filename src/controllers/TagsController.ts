168346117a5c661bb2958d7956537d0ebe30ed49
mixtyraa <mixtyraa@mail.ru>
2019-04-06 19:37:16 +0500

    TagController

--- src/controllers/TagsController.ts
+++ src/controllers/TagsController.ts
@@ -1,13 +1,33 @@
-import {Request, Response} from 'express';
+import { knex } from 'database/knex';
+import { Request } from 'express';
+import { IResponse } from 'middleware/Response';
 
 export default class TagsController {
 
-    public static getTags(req: Request, res: Response) {
-        res.json('get Tags');
+    public static getTags(req: Request, res: IResponse) {
+        const knexquery = knex('tags').select('id', 'name');
+        if (req.query.id) {
+            knexquery.where('id', req.query.id);
+        }
+        knexquery.then((data) => {
+            if (data.length > 0) {
+                res.success(data);
+            } else {
+                res.error(404);
+            }
+        });
     }
 
-    public static addTag(req: Request, res: Response) {
-        res.json('add Tag');
+    public static addTag(req: Request, res: IResponse) {
+        if (req.body.name) {
+            knex('tags').insert({
+                name : req.body.name
+            }).then((data) => {
+                knex('tags').select('id', 'name').where('id', data).then((tag) => {
+                    res.success(tag);
+                });
+            });
+        }
     }
 
 }
