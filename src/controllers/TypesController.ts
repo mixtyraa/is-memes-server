e0f158ff99c2271879d3419886daf80ae87eade2
mixtyraa <mixtyraa@mail.ru>
2019-04-06 19:37:00 +0500

    TypesController

--- src/controllers/TypesController.ts
+++ src/controllers/TypesController.ts
@@ -1,13 +1,33 @@
-import {Request, Response} from 'express';
+import { knex } from 'database/knex';
+import { Request } from 'express';
+import { IResponse } from 'middleware/Response';
 
 export default class TypeController {
 
-    public static getTypes(req: Request, res: Response) {
-        res.json('get types');
+    public static getTypes(req: Request, res: IResponse) {
+        const knexquery = knex('types').select('id', 'name');
+        if (req.query.id) {
+            knexquery.where('id', req.query.id);
+        }
+        knexquery.then((data) => {
+            if (data.length > 0) {
+                res.success(data);
+            } else {
+                res.error(404);
+            }
+        });
     }
 
-    public static addType(req: Request, res: Response) {
-        res.json('add type');
+    public static addType(req: Request, res: IResponse) {
+        if (req.body.name) {
+            knex('types').insert({
+                name : req.body.name
+            }).then((data) => {
+                knex('types').select('id', 'name').where('id', data).then((tag) => {
+                    res.success(tag);
+                });
+            });
+        }
     }
 
 }
