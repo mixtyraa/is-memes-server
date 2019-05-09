957599dacf656e799ee1f5d35d59b46f126ff6b0
mixtyraa <mixtyraa@mail.ru>
2019-05-09 17:07:14 +0500

    Fixed CORS Policy

--- src/middleware/AllowCrossDomain.ts
+++ src/middleware/AllowCrossDomain.ts
@@ -2,5 +2,7 @@ import {Request, Response} from 'express';
 
 export default function(req: Request, res: Response, next: CallableFunction): void {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 }
